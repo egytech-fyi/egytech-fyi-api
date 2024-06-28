c6ba0420c1a3ed599ecdbc4f9e18b0f79fb25e60
iahmedgamal <ahmad.g.mustafa@gmail.com>
2024-06-29 01:16:14 +0300

    added a new route to reterive the repos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Hono } from 'hono';
 import participantsHandler from './participants';
 import statsHandler from './stats';
+import reposHandler from './repos';
 import { Bindings } from '../types/bindings';
 
 const routes = (app: Hono<{ Bindings: Bindings }>) => {
   app.get('/participants', participantsHandler);
   app.get('/stats', statsHandler);
+  app.get('/repos',reposHandler)
 };
 
 export default routes;
